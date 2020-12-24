1a2b997935c1ddc45c0384f71fa8b7288f087788
PhuongNgan2103 <nguyenphuongngan2103@gmail.com>
2020-12-24 10:08:13 +0700

    lesson8-array-and-linkedlist

---
+++ bai-tap/Deployment-method-arrayList/MyList.php
@@ -0,0 +1,69 @@
+<?php
+
+class MyList
+{
+    public $size;
+    public $elements;
+
+    /**
+     * MyList constructor.
+     * @param $size
+     * @param $elements
+     */
+    public function __construct($size, $elements)
+    {
+        $this->size = $size;
+        $this->elements = $elements;
+    }
+
+    public function insert($index, $obj)
+    {
+
+    }
+
+    public function add($obj)
+    {
+
+    }
+
+    public function remove($index)
+    {
+
+    }
+
+    public function get($index)
+    {
+
+    }
+
+    public function clean()
+    {
+
+    }
+
+    public function addAll($arr)
+    {
+
+    }
+
+    public function indexOf($obj)
+    {
+
+    }
+
+    public function isEmpty()
+    {
+
+    }
+
+    public function sort()
+    {
+
+    }
+
+    public function reset()
+    {
+
+    }
+
+}
---
+++ thuc-hanh/Deployment-class-list/ArrayList.php
@@ -0,0 +1,43 @@
+<?php
+
+
+class ArrayList
+{
+    public $arrayList;
+
+    /**
+     * ArrayList constructor.
+     * @param $arrayList
+     */
+    public function __construct($arrayList)
+    {
+        $this->arrayList = $arrayList;
+    }
+
+    public function ArrayList($arr = "")
+    {
+        if (is_array($arr) == true)
+            $this->arrayList = $arr;
+        else
+            $this->arrayList = array();
+    }
+
+    public function add($obj)
+    {
+        array_push($this->arrayList, $obj);
+    }
+
+    public function get($index)
+    {
+        if ($this->isInteger($index) && $index < $this->size()) {
+            return $this->arrayList[$index];
+        } else {
+            die("ERROR in ArrayList.get");
+        }
+    }
+
+    public function isInteger($toCheck) {
+        return preg_match("/^[0-9]+$/", $toCheck);
+    }
+
+}
